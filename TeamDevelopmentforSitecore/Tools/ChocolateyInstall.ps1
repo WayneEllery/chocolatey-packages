656ab463b229052e8a12736eac53623f04ed655d
Wayne Ellery <WayneEllery@users.noreply.github.com>
2015-02-25 09:15:28 +1100

    Modified to use temp directory instead for zip file, added TDS icon

--- TeamDevelopmentforSitecore/Tools/ChocolateyInstall.ps1
+++ TeamDevelopmentforSitecore/Tools/ChocolateyInstall.ps1
@@ -1,11 +1,11 @@
 $packageName = "TDS"
 $downloadUrl = "http://www.hhogdev.com/~/media/Files/Products/Team_Development/HedgehogDevelopmentTDS.zip"
 $silentArgs = "/quiet"
-$toolsDir = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
+$zipDownloadDirectory = "$($env:TEMP)/TDS'
 $regVs2010 = "HKCR:\VisualStudio.DTE.10.0"
 $regVs2012 = "HKCR:\VisualStudio.DTE.11.0"
 $regVs2013 = "HKCR:\VisualStudio.DTE.12.0"
-$tdsSetupDirectory = "$toolsDir/5.1.0.3/"
+$tdsSetupDirectory = "$zipDownloadDirectory/5.1.0.3/"
 
 $tdsVs2010SetupFile = "$($tdsSetupFileBasePath)VS2010.msi"
 $tdsVs2012SetupFile = "$($tdsSetupFileBasePath)HedgehogDevelopmentTDS_VS2012.msi"
@@ -21,7 +21,7 @@ if (-Not $vs2010Installed -Or -Not $vs2012Installed -Or -Not $vs2012Installed) {
 	throw "No compatible Visual studio installed. Checked for Visual Studio 2010, 2012 and 2013."
 }
 
-Install-ChocolateyZipPackage "$packageName" "$downloadUrl" "$toolsDir"
+Install-ChocolateyZipPackage "$packageName" "$downloadUrl" "$zipDownloadDirectory"
 
 Function InstallMsi ($tdsSetupDirectory, $vsVersion, $packageName, $silentArgs) {
 	$setupFile = "$($tdsSetupDirectory)HedgehogDevelopmentTDS$vsVersion.msi"
